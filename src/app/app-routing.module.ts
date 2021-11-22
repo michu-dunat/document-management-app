a7019aa1e8ea5ba604b7d27cdc877ed5624fb01f
michu-dunat <kontaktmichaldunat@gmail.com>
2021-11-22 14:21:58 +0100

    add material module and imports, add toolbar, add case component, add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaseComponent } from './components/case/case.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'case', component: CaseComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
